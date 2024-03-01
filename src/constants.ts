cfb6681d7469ddf52f1dbe0c9f7ffb592d84b933
Sweeney Ngo <sweeneyngo@gmail.com>
2024-02-29 22:55:30 -0800

    Fix environment typo

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const DEBOUNCE_MSEC = 195;
 export const SEARCH_THRESHOLD = 0.65;
 export const VERSION = "v0.3.0";
-export const ENVIROMENT = "production";
+export const ENVIRONMENT = "production";
